d569dea7b822fdfea689bf30098fab9c843eff37
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-04-25 01:45:22 -0300

    mobile rpg initial structure

---
+++ Enemy.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var hpLabel = $HPLabel
+onready var animation = $AnimationPlayer
+
+signal died
+
+var hp = 25 setget set_hp
+
+func set_hp(new_hp):
+	hp = new_hp
+	hpLabel.text = str(hp) + "hp"
+	if hp <= 0:
+		emit_signal("died")
+		queue_free()
+	else:
+		animation.play("Shake")
+		yield(animation, "animation_finished")
+		animation.play("Attack")
