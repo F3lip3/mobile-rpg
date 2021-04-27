b6c8e5bbd41bb2ff9ddf0f0c7c82f60a3a580a99
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-04-27 10:38:06 -0300

    creates enemies dynamically

--- Battle.gd
+++ Battle.gd
@@ -2,13 +2,21 @@ extends Node
 
 const BattleUnits = preload("res://BattleUnits.tres")
 
+export(Array, PackedScene) var enemies = []
+
 onready var buttons = $UI/BattleActionButtons
+onready var fadeAnimation = $FadeAnimation
+onready var nextEnemyButton = $UI/CenterContainer/NextEnemyButton
+onready var enemyPosition = $EnemyPosition
 
 func _ready():
+	randomize()
+	nextEnemyButton.hide()
+	create_new_enemy()
 	start_player_turn()
 
 func start_enemy_turn():
-	buttons.hide()
+	buttons.hide()	
 	var enemy = BattleUnits.Enemy
 	if enemy != null:
 		enemy.attack()
@@ -23,5 +31,22 @@ func start_player_turn():
 		yield(player, "end_turn")
 		start_enemy_turn()
 
+func create_new_enemy():
+	enemies.shuffle()
+	var Enemy = enemies.front()
+	var enemy = Enemy.instance()
+	enemyPosition.add_child(enemy)
+	enemy.connect("died", self, "_on_Enemy_died")
+
 func _on_Enemy_died():
+	nextEnemyButton.show()
 	buttons.hide()
+
+func _on_NextEnemyButton_pressed():
+	nextEnemyButton.hide()
+	fadeAnimation.play("FadeToNewEnemy")
+	yield(fadeAnimation, "animation_finished")
+	var player = BattleUnits.Player
+	player.ap = player.max_ap
+	buttons.show()
+	create_new_enemy()
--- Enemy.gd
+++ Enemy.gd
@@ -8,11 +8,13 @@ onready var animation = $AnimationPlayer
 signal died
 signal end_turn
 
-var hp = 25 setget set_hp
+export(int) var hp = 25 setget set_hp
+export(int) var damage = 4
 
 func set_hp(new_hp):
 	hp = new_hp
-	hpLabel.text = str(hp) + "hp"
+	if hpLabel != null:
+		hpLabel.text = str(hp) + "hp"
 
 func attack() -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
@@ -21,7 +23,7 @@ func attack() -> void:
 	emit_signal("end_turn")
 
 func deal_damage():
-	BattleUnits.Player.hp -= 4
+	BattleUnits.Player.hp -= damage
 
 func is_dead():
 	return hp <= 0
